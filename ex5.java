4632e432739982809c6e9cfee349a28d9afea987
Taron Nersisyan <31661360+7aron@users.noreply.github.com>
2018-02-12 21:35:19 +0400

    Create ex5.java

---
+++ ex5.java
@@ -0,0 +1,22 @@
+import java.util.Random;
+import java.util.Scanner;
+
+class ex5 { 
+public static void main (String[] args){
+int a = 0;
+int b = 0;
+int[][] grades = new int[a][b];
+
+Scanner sc = new Scanner(System.in);
+System.out.println("NUmber of rows");
+a = sc.nextInt();
+System.out.println("NUmber of columns");
+b = sc.nextInt();
+
+
+if (a < b || a > b) {
+System.out.println("It is not a square, check your numbers of rows and columns");
+} else System.out.println("This is a square now.");
+
+}
+}
